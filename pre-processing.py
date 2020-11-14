57bff9be5c24cfb381f3a5dd323cd23955151009
Yujian <yujian_fu@outlook.com>
2020-11-15 06:41:31 +0800

    add pre-processing

--- pre-processing.py
+++ pre-processing.py
@@ -10,7 +10,7 @@ start = time.time()
 action_list = ['view', 'cart', 'remove_from_cart', 'purchase']
 
 FileFolderPath = "./dataset/"
-FileName = FileFolderPath + "2019-Nov.csv"
+FileName = FileFolderPath + "2019-Oct.csv"
 user_dict_view = {}
 user_dict_buy = {}
 user_dict_add_chart = {}
