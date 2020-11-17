6e0577d1a894ea4547a73555302fcde8f4328259
Yujian <yujian_fu@outlook.com>
2020-11-18 00:44:41 +0800

    CF item based

--- select_subset.py
+++ select_subset.py
@@ -3,7 +3,7 @@ from config import *
 
 with open(FileFolderPath + FileNameList[0]) as f:
     reader = list(csv.reader(f))
-    with open(FileFolderPath + FileNameList[0].split('.')[0] + "-small.csv") as wf:
+    with open(FileFolderPath + FileNameList[0].split('.')[0] + "-small.csv", "w", newline="") as wf:
         writer = csv.writer(wf)
         print("Writing ", int(len(reader)/5), " lines")
         writer.writerow(reader[: int(len(reader)/5)])
