6aa3232211c40eef35ce7a2640be94011eeaf084
Yujian <yujian_fu@outlook.com>
2020-11-18 00:56:16 +0800

    CF item based

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ VIS_NUM = 10
 VIS_K = 10
 ActionList = ['view', 'cart', 'remove_from_cart', 'purchase']
 FileFolderPath = "./dataset/"
-FileNameList = ["2019-Dec-sub.csv", "2019-Oct.csv"]
+FileNameList = ["2019-Dec-sub-small.csv", "2019-Oct.csv"]
