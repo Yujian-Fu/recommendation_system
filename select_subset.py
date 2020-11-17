4b3a728067b89053315904f7aadc495689e3b396
Yujian <yujian_fu@outlook.com>
2020-11-18 00:52:31 +0800

    CF item based

--- select_subset.py
+++ select_subset.py
@@ -6,7 +6,7 @@ with open(FileFolderPath + FileNameList[0]) as f:
     with open(FileFolderPath + FileNameList[0].split('.')[0] + "-small.csv", "w", newline="") as wf:
         writer = csv.writer(wf)
         print("Writing ", int(len(reader)/5), " lines")
-        for idx in len(reader):
+        for idx in range(len(reader)):
             writer.writerow(reader[idx])
             if idx > int(len(reader)/5):
                 break
